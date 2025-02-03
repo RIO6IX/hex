a3b7d16146f74d9c1cdaa82c3f7661829ab5bdca
DayanHesara <dayanhesara0@gmail.com>
2025-02-04 01:06:24 +0530

    USS banner

--- Js/popup.js
+++ Js/popup.js
@@ -2,10 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const popupBanner = document.getElementById('popupBanner');
     const popupClose = document.querySelector('.popup-close');
 
-    // Show the banner after 3 seconds
-    setTimeout(() => {
-        popupBanner.style.display = 'flex';
-    }, 3000);
+  
 
     // Close popup when clicking the close button
     popupClose.addEventListener('click', () => {
